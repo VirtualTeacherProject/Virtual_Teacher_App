6fed90bd019f255e82a07966c7fdb858434a6856
UMUT YILDIRIM <yolgezerestel@gmail.com>
2024-11-19 21:21:17 +0200

    Initial commit

---
+++ src/main/java/com/MarianFinweFeanor/Virtual_Teacher/VirtualTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.MarianFinweFeanor.Virtual_Teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualTeacherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MarianFinweFeanor/Virtual_Teacher/VirtualTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MarianFinweFeanor.Virtual_Teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
