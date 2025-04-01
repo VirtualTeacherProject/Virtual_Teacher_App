441acf76232cafd2eb1e935ee92afaec31dcec43
UMUT YILDIRIM <yolgezerestel@gmail.com>
2025-04-02 00:11:14 +0300

    h2 database update
    some bug fixing
    important note: h2 database driver library only works with h2-2.1.214.jar

--- src/main/java/com/MarianFinweFeanor/Virtual_Teacher/Config/HibernateConfig.java
+++ src/main/java/com/MarianFinweFeanor/Virtual_Teacher/Config/HibernateConfig.java
@@ -16,38 +16,38 @@ import java.util.Properties;
 @PropertySource("classpath:application.properties")
 public class HibernateConfig {
 
-    private final String dbUrl, dbUsername, dbPassword;
+//    private final String dbUrl, dbUsername, dbPassword;
+//
+//    @Autowired
+//    public HibernateConfig(Environment env) {
+//        this.dbUrl = env.getProperty("database.url");
+//        this.dbUsername = env.getProperty("database.username");
+//        this.dbPassword = env.getProperty("database.password");
+//    }
 
-    @Autowired
-    public HibernateConfig(Environment env) {
-        this.dbUrl = env.getProperty("database.url");
-        this.dbUsername = env.getProperty("database.username");
-        this.dbPassword = env.getProperty("database.password");
-    }
+//    @Bean(name = "entityManagerFactory")
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(datasource());
+//        sessionFactory.setPackagesToScan("com.MarianFinweFeanor.Virtual_Teacher");
+//        sessionFactory.setHibernateProperties(hibernateProperties());
+//        return sessionFactory;
+//    }
+//
+//
+//    @Bean
+//    public DataSource datasource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl(dbUrl);
+//        dataSource.setUsername(dbUsername);
+//        dataSource.setPassword(dbPassword);
+//        return dataSource;
+//    }
 
-    @Bean(name = "entityManagerFactory")
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(datasource());
-        sessionFactory.setPackagesToScan("com.MarianFinweFeanor.Virtual_Teacher");
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
-
-
-    @Bean
-    public DataSource datasource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl(dbUrl);
-        dataSource.setUsername(dbUsername);
-        dataSource.setPassword(dbPassword);
-        return dataSource;
-    }
-
-    public Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        return hibernateProperties;
-    }
+//    public Properties hibernateProperties() {
+//        Properties hibernateProperties = new Properties();
+//        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+//        return hibernateProperties;
+//    }
 }
