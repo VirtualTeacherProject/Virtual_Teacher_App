6349ebbf0a716f323850c4a2504c9624fc8a19c0
UMUT YILDIRIM <yolgezerestel@gmail.com>
2025-04-06 16:11:46 +0300

    coursecontroller
    coursecontroller

---
+++ src/main/java/com/MarianFinweFeanor/Virtual_Teacher/Controller/CourseController.java
@@ -0,0 +1,57 @@
+ package com.MarianFinweFeanor.Virtual_Teacher.Controller;
+
+import com.MarianFinweFeanor.Virtual_Teacher.Model.Course;
+import com.MarianFinweFeanor.Virtual_Teacher.Model.User;
+import com.MarianFinweFeanor.Virtual_Teacher.Service.CourseService;
+import com.MarianFinweFeanor.Virtual_Teacher.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    //Create or Update a course
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody Course course) {
+        try {
+            Course saveCourse = courseService.createCourse(course);
+            return ResponseEntity.status(HttpStatus.CREATED).body(saveCourse);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+        }
+    }
+
+    // Get all courses
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courses = courseService.getAllCourses();
+        return ok(courses);
+    }
+
+    // Get a course by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Delete a course by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+        return ResponseEntity.noContent().build();
+
+
+    }
+}
