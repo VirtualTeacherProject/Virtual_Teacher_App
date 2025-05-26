162d5f65aa6233dcd3207cff6434f02923f8949f
Marian <marianmgm@yahoo.com>
2025-05-26 08:58:00 +0300

    finish exception handling in LecturesRestController

--- src/main/java/com/MarianFinweFeanor/Virtual_Teacher/Controller/RestController/LecturesRestController.java
+++ src/main/java/com/MarianFinweFeanor/Virtual_Teacher/Controller/RestController/LecturesRestController.java
@@ -2,6 +2,7 @@ package com.MarianFinweFeanor.Virtual_Teacher.Controller.RestController;
 
 import com.MarianFinweFeanor.Virtual_Teacher.Model.Lecture;
 import com.MarianFinweFeanor.Virtual_Teacher.Service.Interfaces.LectureService;
+import com.MarianFinweFeanor.Virtual_Teacher.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,18 +19,30 @@ public class LecturesRestController {
     }
     @GetMapping
     public List<Lecture> getAllLectures() {
-        return lectureService.getAllLectures();
+        try {
+            return lectureService.getAllLectures();
+        }
+        catch (EntityNotFoundException e){
+            throw new ResponseStatusException(
+                    org.springframework.http.HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
     @GetMapping("/{id}")
     public Lecture getLectureById(@PathVariable Long id) {
-        return lectureService.getLecturesById(id).orElseThrow(()->new ResponseStatusException(
-                org.springframework.http.HttpStatus.NOT_FOUND, String.format("Lecture with id %d not found", id)));
+        try {
+            return lectureService.getLecturesById(id).orElseThrow();
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(
+                    org.springframework.http.HttpStatus.NOT_FOUND, e.getMessage());
+        }
+
     }
     @PostMapping
     public Lecture createLecture(@RequestBody Lecture lecture) {
         try {
             return lectureService.createLectures(lecture);
-        } catch (Exception e) {
+        } catch (Exception e) { //todo fix once filtering is done
             throw new ResponseStatusException(
                     org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, "Error creating lecture", e);
         }
@@ -39,18 +52,18 @@ public class LecturesRestController {
     public Lecture updateLecture(@PathVariable Long id, @RequestBody Lecture updatedLecture) {
         try {
             return lectureService.updateLecture(id, updatedLecture);
-        } catch (Exception e) {
+        } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(
-                    org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, "Error updating lecture", e);
+                    org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
     @DeleteMapping("/{id}")
     public void deleteLecture(@PathVariable Long id) {
         try {
             lectureService.delete(id);
-        } catch (Exception e) {
+        } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(
-                    org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, "Error deleting lecture", e);
+                    org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 
